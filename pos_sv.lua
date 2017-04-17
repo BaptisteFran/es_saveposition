2403d4bc8d55152ce054997b5a79164892409612
Kerstern <baptiste.franel@gmail.com>
2017-04-17 20:34:41 +0200

    Update pos_sv.lua

--- pos_sv.lua
+++ pos_sv.lua
@@ -6,14 +6,9 @@ AddEventHandler("es:coords", function( PlayerName , x , y , z )
         MySQL:executeQuery("UPDATE user_position SET x_pos='@x' WHERE identifier = '@username' AND y_pos = '@y' AND z_pos = '@z'",{['@usersame'] = player, ['@x'] = x, ['@y'] = y, ['@z'] = z})
                 end)
 
-local function savePlayerCoords()
-    SetTimeout(1000, function()
-        TriggerEvent("es:savepos", function(users)
-                for k,v in pairs(users)do
-                    MySQL:executeQuery("UPDATE user_position SET x_pos='@x' WHERE identifier = '@username' AND y_pos = '@y' AND z_pos = '@z'",{['@usersame'] = player, ['@x'] = x, ['@y'] = y, ['@z'] = z})
-                        end
-                    
-                    end)
-            savePlayerCoords()
+Citizen.CreateThread(function ()
+    while true do
+    Citizen.Wait(1000)
+        TriggerServerEvent('es:coords')
+            end
         end)
-    end
