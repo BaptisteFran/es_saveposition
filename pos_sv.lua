4a4db59f7e3ee8022c0a048af061d3216362ddfc
Kerstern <baptiste.franel@gmail.com>
2017-04-17 20:16:51 +0200

    Update pos_sv.lua

--- pos_sv.lua
+++ pos_sv.lua
@@ -1,5 +1,5 @@
 require "resources/essentialmode/lib/MySQL"
-MySQL:open("localhost", "gta5_gamemode_essential", "root", "fanny123")
+MySQL:open("localhost", "gta5_gamemode_essential", "root", "password")
 
 RegisterServerEvent("es:coords")
 AddEventHandler("es:coords", function( PlayerName , x , y , z )
