a1a46c482d71662a08e15cd87d948ea61bd610f2
Kerstern <baptiste.franel@gmail.com>
2017-04-18 10:10:35 +0200

    Update pos_sv.lua

--- pos_sv.lua
+++ pos_sv.lua
@@ -2,8 +2,8 @@ require "resources/essentialmode/lib/MySQL"
 MySQL:open("localhost", "gta5_gamemode_essential", "root", "password")
 
 RegisterServerEvent("es:coords")
-AddEventHandler("es:coords", function( PlayerName , x , y , z )
-        MySQL:executeQuery("UPDATE user_position SET x_pos='@x' WHERE identifier = '@username' AND y_pos = '@y' AND z_pos = '@z'",{['@usersame'] = player, ['@x'] = x, ['@y'] = y, ['@z'] = z})
+AddEventHandler("es:coords", function( PlayerId , x , y , z )
+        MySQL:executeQuery("UPDATE user_position SET x_pos='@x' WHERE identifier = '@username' AND y_pos = '@y' AND z_pos = '@z'",{['@usersame'] = PlayerId, ['@x'] = x, ['@y'] = y, ['@z'] = z})
                 end)
 
 Citizen.CreateThread(function ()
