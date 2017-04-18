78675d6a1e3f78c7d5b958c31c62c86fa956177e
Kerstern <baptiste.franel@gmail.com>
2017-04-18 10:02:17 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,8 @@
+-- Manifest
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+-- Requiring essentialmode
+dependency 'essentialmode'
 
 server_script 'pos_sv.lua'
 client_script 'pos_cl.lua'
