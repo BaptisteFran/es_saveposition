59e6b04a480bf4b71e5eac9f784cce55f65f7952
Kerstern <baptiste.franel@gmail.com>
2017-04-19 10:06:57 +0200

    Update pos_sv.lua

--- pos_sv.lua
+++ pos_sv.lua
@@ -9,7 +9,7 @@ MySQL:open("localhost", "gta5_gamemode_essential", "root", "password")
 
 --[[RegisterServerEvent('firstcoords')
 AddEventHandler('firstcoords', function(player, x, y, z)
-	MySQL:executeQuery("INSERT INTO user_position (identifier, x_pos, y_pos, z_pos) VALUES ('@username', '@x', '@y', '@z'),{['@username'] = player, ['@x'] = 0.0, ['@y'] = 0.0, ['@z'] = 0.0})
+	MySQL:executeQuery("INSERT INTO user_position (username, x_pos, y_pos, z_pos) VALUES ('@username', '@x', '@y', '@z'),{['@username'] = player, ['@x'] = 0.0, ['@y'] = 0.0, ['@z'] = 0.0})
 			end)]]--
 
 
@@ -17,7 +17,7 @@ RegisterServerEvent('es:coords')
 AddEventHandler('es:coords', function(player, x, y, z)
 	--if(Users[source])then
 		--Users[source]:setCoords(x, y, z)
-	        MySQL:executeQuery("INSERT INTO user_position (identifier,x_pos,y_pos,z_pos) VALUES ('@username','@x','@y','@z') ON DUPLICATE KEY UPDATE x_pos='@x', y_pos='@y', z_pos='@z';",{['@usersame'] = player, ['@x'] = x, ['@y'] = y, ['@z'] = z})
+	        MySQL:executeQuery("INSERT INTO user_position (username,x_pos,y_pos,z_pos) VALUES ('@username','@x','@y','@z') ON DUPLICATE KEY UPDATE x_pos='@x', y_pos='@y', z_pos='@z';",{['@usersame'] = player, ['@x'] = x, ['@y'] = y, ['@z'] = z})
                 end)
 
 	end
