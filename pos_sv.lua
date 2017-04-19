8f1718e17fc0a7ed30d7ab21c645f406a1c403fb
Kerstern <baptiste.franel@gmail.com>
2017-04-19 09:42:07 +0200

    Update pos_sv.lua

--- pos_sv.lua
+++ pos_sv.lua
@@ -5,13 +5,10 @@ MySQL:open("localhost", "gta5_gamemode_essential", "root", "password")
 --AddEventHandler("es:coords", function( PlayerId , x , y , z )
 
 RegisterServerEvent('es:coords')
-AddEventHandler('es:coords', function(x, y, z)
-		
-		local player = GetPlayerPed(-1)
+AddEventHandler('es:coords', function(player, x, y, z)
 	--if(Users[source])then
 		--Users[source]:setCoords(x, y, z)
-	
-                MySQL:executeQuery("INSERT INTO user_position (identifier,x_pos,y_pos,z_pos) VALUES ('@username','@x','@y','@z') ON DUPLICATE KEY UPDATE x_pos='@x', y_pos='@y', z_pos='@z';",{['@usersame'] = player, ['@x'] = x, ['@y'] = y, ['@z'] = z})
+	        MySQL:executeQuery("INSERT INTO user_position (identifier,x_pos,y_pos,z_pos) VALUES ('@username','@x','@y','@z') ON DUPLICATE KEY UPDATE x_pos='@x', y_pos='@y', z_pos='@z';",{['@usersame'] = player, ['@x'] = x, ['@y'] = y, ['@z'] = z})
                 end)
 
 	end
