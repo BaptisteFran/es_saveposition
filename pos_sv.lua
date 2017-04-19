35ef8ea6c8d0dad5318ef3437a2dc13df606631e
Kerstern <baptiste.franel@gmail.com>
2017-04-19 09:56:33 +0200

    Update pos_sv.lua

--- pos_sv.lua
+++ pos_sv.lua
@@ -4,6 +4,15 @@ MySQL:open("localhost", "gta5_gamemode_essential", "root", "password")
 --RegisterServerEvent("es:coords")
 --AddEventHandler("es:coords", function( PlayerId , x , y , z )
 
+
+-- Should i start with this ?
+
+--[[RegisterServerEvent('firstcoords')
+AddEventHandler('firstcoords', function(player, x, y, z)
+	MySQL:executeQuery("INSERT INTO user_position (identifier, x_pos, y_pos, z_pos) VALUES ('@username', '@x', '@y', '@z'),{['@username'] = player, ['@x'] = 0.0, ['@y'] = 0.0, ['@z'] = 0.0})
+			end)]]--
+
+
 RegisterServerEvent('es:coords')
 AddEventHandler('es:coords', function(player, x, y, z)
 	--if(Users[source])then
