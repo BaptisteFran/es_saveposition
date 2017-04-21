afe7e3fe6c16ca9560b35560b29c27c23bc92a59
Kerstern <baptiste.franel@gmail.com>
2017-04-21 10:26:33 +0200

    Update pos_sv.lua

--- pos_sv.lua
+++ pos_sv.lua
@@ -15,8 +15,8 @@ AddEventHandler('firstcoords', function(player, x, y, z)
 
 RegisterServerEvent('es:coords')
 AddEventHandler('es:coords', function(player, x, y, z)
-	--if(Users[source])then
-		--Users[source]:setCoords(x, y, z)
+	if(Users[source])then
+		Users[source]:setCoords(x, y, z)
 	        MySQL:executeQuery("INSERT INTO user_position (username,x_pos,y_pos,z_pos) VALUES ('@username','@x','@y','@z') ON DUPLICATE KEY UPDATE x_pos='@x', y_pos='@y', z_pos='@z';",{['@usersame'] = player, ['@x'] = x, ['@y'] = y, ['@z'] = z})
                 end)
 
